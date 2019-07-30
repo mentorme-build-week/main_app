93f19c24fd797308bee72526c1e40aa47d6415d1
anthonyvigz <anthonyvigz@gmail.com>
2019-07-30 18:27:28 -0400

    setting up main app page

--- account/src/components/Login.js
+++ account/src/components/Login.js
@@ -4,16 +4,36 @@ class Login extends Component {
     constructor() {
         super();
         this.state =  {
-
+            user: '',
+            password: ''
         }
     }
 
+    handleChange = event => {
+        event.preventDefault();
+
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    signIn = event => {
+        event.preventDefault();
+
+    }
+
     render() {
+        const { user, password } = this.state;
+
         return(
             <div className="signin">
                 <h1>Sign In</h1>
                 <div>portrait placeholder</div>
-                
+                <form className="signinform" onSubmit={this.signIn}>
+                    <input type="password" value={password} placeholder="Password" onChange={this.handleChange} name="password" />
+                    <button type="submit">Sign In</button>
+                </form>
+                <h5>Switch account</h5>
             </div>
         )
     }
